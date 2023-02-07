a7c9a681baf7aa6314e379e7b26955c674941927
Josh Bello <superturboturtle@gmail.com>
2023-02-06 21:43:54 -0700

    Add project files.

---
+++ Models/gradeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission5.Models
+{
+    public class gradeModel
+    {
+        //Validation 
+        [Range(1,100,ErrorMessage ="Enter a score between 1 and 100 for assignments")]
+        public int asg { get; set; }
+        [Range(1, 100, ErrorMessage = "Enter a score between 1 and 100 for group projects")]
+        public int group { get; set; }
+        [Range(1, 100, ErrorMessage = "Enter a score between 1 and 100 for quizes")]
+        public int quiz { get; set; }
+        [Range(1, 100, ErrorMessage = "Enter a score between 1 and 100 for midterm")]
+        public int mt { get; set; }
+        [Range(1, 100, ErrorMessage = "Enter a score between 1 and 100 for final")]
+        public int fn { get; set; }
+        [Range(1, 100, ErrorMessage = "Enter a score between 1 and 100 for intex")]
+        public int itx { get; set; }
+
+    }
+}
---
+++ wwwroot/js/Grades.js
@@ -0,0 +1,37 @@
+﻿
+$("#btnCalc").click(function () {
+    //get variables from html page
+    var as = $("#txtAssignments").val() * .5
+    var grp = $("#txtGroup").val() * .1
+    var quiz = $("#txtQuiz").val() * .1
+    var mt = $("#txtMidterm").val() * .1
+    var fn = $("#txtFinal").val() * .1
+    var itx = $("#txtINTEX").val() * .1
+   
+
+    // varables for final percent and letter
+    var fnGrdPct = as + grp + quiz + mt + fn + itx
+    var fnGrdLtr
+
+    //prepare variables and arrays for grade calclation
+    var x = 0
+    var y = 0
+    const letters = ['A','A-','B+','B','B-','C+','C', 'C-', 'D+','D','D-','E']
+    const percentages = [94,90,87,84,80,77,74,70,67,64,60,0]
+
+    //calculate letter grade
+    while (x < 1) {
+        if (fnGrdPct >= percentages[y]) {
+            fnGrdLtr = letters[y]
+            x++
+        }
+        else {
+            y++
+        }
+    }
+
+    //return html with grade
+    //$('#fGrade').html('Letter Grade: ' + fnGrdLtr + '<br/>Percentage: ' + fnGrdPct)
+    window.alert('Letter Grade: ' + fnGrdLtr + '\nPercentage: ' + fnGrdPct)
+
+})
